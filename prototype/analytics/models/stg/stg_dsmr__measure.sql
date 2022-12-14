91399dad430d6536dd0e17db8a222215ab87c5f1
zsombor-flds <zsombor.foldesi@hiflylabs.com>
2022-12-14 18:01:02 +0100

    fix casting

--- prototype/analytics/models/stg/stg_dsmr__measure.sql
+++ prototype/analytics/models/stg/stg_dsmr__measure.sql
@@ -9,15 +9,15 @@ renamed as (
   select
     cast(power_timestamp as timestamp) as measure_timestamp,
     power_device_name,
-    cast(substring(power_consumption, 0, 11) as float) as power_consumption_khw,
-    cast(substring(l1_phase_voltage, 0, 6) as float) as l1_phase_voltage,
-    cast(substring(l2_phase_voltage, 0, 6) as float) as l2_phase_voltage,
-    cast(substring(l3_phase_voltage, 0, 6) as float) as l3_phase_voltage,
-    cast(substring(l1_phase_current, 0, 4) as float) as l1_phase_current,
-    cast(substring(l2_phase_current, 0, 4) as float) as l2_phase_current,
-    cast(substring(l3_phase_current, 0, 4) as float) as l3_phase_current,
-    cast(humidity as float) as humidity,
-    cast(temperature as float) as temperature
+    cast(substring(power_consumption, 0, 11) as numeric) as power_consumption_khw,
+    cast(substring(l1_phase_voltage, 0, 6) as numeric) as l1_phase_voltage,
+    cast(substring(l2_phase_voltage, 0, 6) as numeric) as l2_phase_voltage,
+    cast(substring(l3_phase_voltage, 0, 6) as numeric) as l3_phase_voltage,
+    cast(substring(l1_phase_current, 0, 4) as numeric) as l1_phase_current,
+    cast(substring(l2_phase_current, 0, 4) as numeric) as l2_phase_current,
+    cast(substring(l3_phase_current, 0, 4) as numeric) as l3_phase_current,
+    cast(humidity as numeric) as outside_humidity,
+    cast(temperature as numeric) as outside_temperature
   from source
 
 )
